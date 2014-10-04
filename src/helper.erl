66d32253104bb7e456f3210556a481689cab211a
drimtajm <angela.johansson@gmail.com>
2014-10-04 16:53:17 +0200

    Add line breaks in debug printout

--- src/helper.erl
+++ src/helper.erl
@@ -3,9 +3,9 @@
 
 trillium_connect() ->
     erlang:set_cookie(node(), 'erlang-rocks'),
-    io:format("net_kernel:connect('pi@192.168.2.2')."),
+    io:format("net_kernel:connect('pi@192.168.2.2').~n"),
     net_kernel:connect('pi@192.168.2.2').
 
 msg(Name) ->
-    io:format("display:show_welcome_message(~p)", [Name]),
+    io:format("display:show_welcome_message(~p).~n", [Name]),
     display:show_welcome_message(Name).
