413dace693e09b39b7955f6c9e6f8a8a79851f57
drimtajm <angela.johansson@gmail.com>
2014-10-04 16:47:42 +0200

    Add helper function for welcome message display
    * Also add debug printout

--- src/helper.erl
+++ src/helper.erl
@@ -3,4 +3,9 @@
 
 trillium_connect() ->
     erlang:set_cookie(node(), 'erlang-rocks'),
+    io:format("net_kernel:connect('pi@192.168.2.2')."),
     net_kernel:connect('pi@192.168.2.2').
+
+msg(Name) ->
+    io:format("display:show_welcome_message(~p)", [Name]),
+    display:show_welcome_message(Name).
