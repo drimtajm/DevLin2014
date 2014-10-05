1df5d0cd318359ff376e1f223fd39c1cfd54d512
drimtajm <angela.johansson@gmail.com>
2014-10-05 11:35:11 +0000

    Change preferences for hogwarts

--- hogwarts_erlang.sh
+++ hogwarts_erlang.sh
@@ -1,3 +1,1 @@
-sudo hciconfig hci0 up
-sudo hciconfig hci0 piscan
-sudo erl -name pi@192.168.2.4 -pa ebin deps/bluetooth/ebin -cookie "erlang-rocks"
+sudo erl -name pi@192.168.2.4 -pa ebin deps/jsx/ebin -cookie "erlang-rocks"
